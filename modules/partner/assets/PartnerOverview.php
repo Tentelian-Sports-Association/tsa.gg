58f128498ffba03abd3ffda60f2f7462b8d0bcec
Birnchen <developer@tsa.gg>
2020-05-19 19:28:06 +0200

    partnerOverviewAssets

---
+++ modules/partner/assets/PartnerOverview.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\modules\partner\assets;
+
+use yii\web\AssetBundle;
+
+class PartnerOverviewAsset extends AssetBundle
+{
+    public $sourcePath = '@app/modules/partner/assets';
+ 
+    public $css = [
+    ];
+
+        public $cssOptions = [
+	];
+}
