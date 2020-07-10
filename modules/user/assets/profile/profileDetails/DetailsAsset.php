3b1048bdf01e2ff1f7c7e5eb73a57d5c70f1b3b8
8bamo <8bamosa@gmail.com>
2020-07-11 01:31:34 +0200

    Update game add and profile detail

--- modules/user/assets/profile/profileDetails/DetailsAsset.php
+++ modules/user/assets/profile/profileDetails/DetailsAsset.php
@@ -9,10 +9,10 @@ class DetailsAsset extends AssetBundle
     public $sourcePath = '@app/assets/dist';
  
     public $css = [
-		'css/profileDetails.css'
+		'css/login.css'
     ];
 
     public $cssOptions = [
-        'async' => 'true'
+        'async' => 'false'
 	];
 }
--- modules/user/views/account/addGame.php
+++ modules/user/views/account/addGame.php
@@ -15,33 +15,44 @@ $this->title = \app\modules\user\Module::t('addGameAccount', 'addGameAccount_hea
 
 ?>
 
-<div class="site-add-gameid">
-    <div class="addGameID">
-        <h1><?= Html::encode($this->title) ?></h1>
+<div class="site-add-gameid container ">
+    <div class="addGameID bg-darkblue-2 p-5 my-5">
+        <h3><?= Html::encode($this->title) ?></h3>
 
         <?php $form = ActiveForm::begin([
             'id' => 'edit-details-form',
             'options' => [ 'class' => 'form-horizontal'],
                 'fieldConfig' => [
                     'template' => "{label}\n<div class=\"col-lg-12\">{input}</div>\n<div class=\"col-lg-12\">{error}</div>",
-                    'labelOptions' => ['class' => 'col-lg-2 control-label'],
+                    'labelOptions' => ['class' => 'col control-label'],
                 ],
             ]); ?>
 
-        
+            <div class="row">
+                <div class="col-md-6">
+					<?= $form->field($model, 'player_id')->textInput(['placeholder' => \app\modules\user\Module::t('addGameAccount', 'addGameAccount_gameAccountPlaceholder'), "class" => 'input-default form-control form-control-color']) ?>
+					<?= $form->field($model, 'game_id')->dropDownList($gamesList, ["class" => 'input-default form-control form-control-color', 'prompt' => \app\modules\user\Module::t('addGameAccount', 'addGameAccount_chooseYourGame')]) ?>
+                </div>
+                <div class="col-md-6">
+					<?= $form->field($model, 'visible')->checkbox() ?>
 
-            <?= $form->field($model, 'player_id')->textInput(['placeholder' => \app\modules\user\Module::t('addGameAccount', 'addGameAccount_gameAccountPlaceholder')],["class" => 'form-control form-control-color']) ?>
-            
-            <?= $form->field($model, 'visible')->checkbox() ?>
+					<?= $form->field($model, 'platform_id')->dropDownList($platformList, ["class" => 'input-default form-control form-control-color', 'prompt' => \app\modules\user\Module::t('addGameAccount', 'addGameAccount_chooseYourPlatform')]) ?>
 
-            <?= $form->field($model, 'game_id')->dropDownList($gamesList, ["class" => 'form-control form-control-color', 'prompt' => \app\modules\user\Module::t('addGameAccount', 'addGameAccount_chooseYourGame')]) ?>
+                </div>
+            </div>
+            <div class="text-right">
+				<?= Html::a(\app\modules\user\Module::t('addGameAccount', 'addGameAccount_backToProfile'), ['user/details', 'userId' => $currentUserID], ['class' => 'outline-btn btn btn-primary delete', 'name' => 'backToProfile-button']); ?>
 
-            <?= $form->field($model, 'platform_id')->dropDownList($platformList, ["class" => 'form-control form-control-color', 'prompt' => \app\modules\user\Module::t('addGameAccount', 'addGameAccount_chooseYourPlatform')]) ?>
+				<?= Html::submitButton(\app\modules\user\Module::t('addGameAccount', 'addGameAccount_save'), ['class' => 'filled-btn btn btn-primary', 'name' => 'saveGameId-button']) ?>
 
-            <?= Html::submitButton(\app\modules\user\Module::t('addGameAccount', 'addGameAccount_save'), ['class' => 'btn btn-primary', 'name' => 'saveGameId-button']) ?>
-            <?= Html::a(\app\modules\user\Module::t('addGameAccount', 'addGameAccount_backToProfile'), ['user/details', 'userId' => $currentUserID], ['class' => 'btn btn-primary delete', 'name' => 'backToProfile-button']); ?>
+            </div>
 
-        <?php ActiveForm::end(); ?>
+
+
+
+
+
+       <?php ActiveForm::end(); ?>
     
     </div>
 </div>
