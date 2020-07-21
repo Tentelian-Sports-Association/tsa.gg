6aaf8f13a1d7e88560225172d0e0edef8ed29a43
Birnchen <developer@tsa.gg>
2020-07-21 10:26:01 +0200

    test for index meta tags

--- modules/miscellaneouse/metaClass.php
+++ modules/miscellaneouse/metaClass.php
@@ -51,5 +51,111 @@ class MetaClass
             //'content' => Yii::$app->HelperClass->checkImage('/images/company/', 'tsa_800x235'),
             'content' => 'https://tsa.gg/images/icons/logo.svg',
         ]); // itemprop image
+
+        /*************** Twitter Card Data ***************/
+        /*$view->registerMetaTag([
+            'name' => 'twitter:card',
+            'content' => 'summary',
+        ]);*/ // twitter:card - summary
+
+        $view->registerMetaTag([
+            'name' => 'twitter:card',
+            'content' => 'summary_large_image',
+        ]); // twitter:card - summary_large_immage
+
+        $view->registerMetaTag([
+            'name' => 'twitter:site',
+            'content' => '@TentelianSA',
+        ]); // twitter:site
+
+        $view->registerMetaTag([
+            'property' => 'twitter:account_id',
+            'content' => '1063431775995727872'
+        ]); // twitter:account_id
+
+        $view->registerMetaTag([
+            'name' => 'twitter:title',
+            'content' => $title,
+        ]); // twitter:title
+
+        $view->registerMetaTag([
+            'name' => 'twitter:description',
+            'content' => 'Tentelian Sports Associationt - Professional Gaming for everyone - Is a Tournament Site for Player from all Countries and all Skill Levels.',
+        ]); // twitter:description - less then 200 characters
+
+        $view->registerMetaTag([
+            'name' => 'twitter:creator',
+            'content' => '@BettyBirnchen',
+        ]); // twitter:creator - author
+
+        $view->registerMetaTag([
+            'name' => 'twitter:image:src',
+            //'content' => Yii::$app->HelperClass->checkImage('/images/company/', 'tsa_800x235'),
+            'content' => 'https://tsa.gg/images/icons/logo.svg',
+        ]); // twitter:image:src
+
+        $view->registerMetaTag([
+            'name' => 'twitter:image:alt',
+            'content' => 'no pic availabel',
+        ]); // twitter:image:alt
+
+        /*************** Open Graph Data (and facebook) ***************/
+        /* Open Graph Data (and facebook) */
+        $view->registerMetaTag([
+            'property' => 'og:title',
+            'content' => $title,
+        ]); // og:title
+
+        $view->registerMetaTag([
+            'property' => 'og:type',
+            'content' => 'website'
+        ]); // og:type
+
+        $view->registerMetaTag([
+            'property' => 'og:url',
+            'content' => 'https://tsa.gg',
+        ]); // og:url
+
+        $view->registerMetaTag([
+            'property' => 'og:image',
+            //'content' => Yii::$app->HelperClass->checkImage('/images/company/', 'tsa_800x235'),
+            'content' => 'https://tsa.gg/images/icons/logo.svg',
+        ]); // og:image
+
+        $view->registerMetaTag([
+            'property' => 'og:image:secure_url',
+            //'content' => Yii::$app->HelperClass->checkImage('/images/company/', 'tsa_800x235'),
+            'content' => 'https://tsa.gg/images/icons/logo.svg',
+        ]); // og:image:secure_url
+
+        $view->registerMetaTag([
+            'property' => 'og:image:type',
+            'content' => 'image/png',
+        ]); // og:image:type
+
+        $view->registerMetaTag([
+            'property' => 'image:width',
+            'content' => '1200',
+        ]); // image:width
+
+        $view->registerMetaTag([
+            'property' => 'image:height',
+            'content' => '630',
+        ]); // image:height
+
+        $view->registerMetaTag([
+            'property' => 'og:image:alt',
+            'content' => 'TSA Logo Large',
+        ]); // og:image:alt
+
+        $view->registerMetaTag([
+            'property' => 'og:description',
+            'content' => 'Tentelian Sports Associationt - Professional Gaming for everyone - Is a Tournament Site for Player from all Countries and all Skill Levels.',
+        ]); // og:description
+
+        $view->registerMetaTag([
+            'property' => 'og:site_name',
+            'content' => $title,
+        ]); // og:sitename
     }
 }
