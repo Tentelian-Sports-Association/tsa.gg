c40affcc753509345245ce01cbd342bb1d949482
Birnchen <developer@tsa.gg>
2020-07-22 16:02:09 +0200

    partner meta und mobile banner

--- modules/miscellaneouse/metaClass.php
+++ modules/miscellaneouse/metaClass.php
@@ -294,4 +294,137 @@ class MetaClass
             'content' => $title,
         ]); // og:sitename
 	}
+
+    public function writeMetaPartner($view, $title, $description)
+    {
+        /*************** standart meta tags ***************/
+        $view->registerMetaTag([
+            'name' => 'description',
+            'content' => $description,
+        ]); // description
+
+        $view->registerMetaTag([
+            'name' => 'author',
+            'content' => 'Tentelian Sports Association',
+        ]); // description
+
+        /*************** Schema.org markup for Google+ ***************/
+        $view->registerMetaTag([
+            'itemprop' => 'name',
+            'content' => $title,
+        ]); // itemprop:name
+
+        $view->registerMetaTag([
+            'itemprop' => 'description',
+            'content' => $description,
+        ]); // itemprop description
+
+        $view->registerMetaTag([
+            'itemprop' => 'image',
+            'content' => Url::base('https') . '/images/metafiles/Partner_524x273.png',
+        ]); // itemprop image
+
+        /*************** Twitter Card Data ***************/
+        /*$view->registerMetaTag([
+            'name' => 'twitter:card',
+            'content' => 'summary',
+        ]);*/ // twitter:card - summary
+
+        $view->registerMetaTag([
+            'name' => 'twitter:card',
+            'content' => 'summary_large_image',
+        ]); // twitter:card - summary_large_immage
+
+        $view->registerMetaTag([
+            'name' => 'twitter:site',
+            'content' => '@TentelianSA',
+        ]); // twitter:site
+
+        $view->registerMetaTag([
+            'property' => 'twitter:account_id',
+            'content' => '1063431775995727872'
+        ]); // twitter:account_id
+
+        $view->registerMetaTag([
+            'name' => 'twitter:title',
+            'content' => $title,
+        ]); // twitter:title
+
+        $view->registerMetaTag([
+            'name' => 'twitter:description',
+            'content' => $description,
+        ]); // twitter:description - less then 200 characters
+
+        $view->registerMetaTag([
+            'name' => 'twitter:creator',
+            'content' => '@BettyBirnchen',
+        ]); // twitter:creator - author
+
+        $view->registerMetaTag([
+            'name' => 'twitter:image:src',
+            'content' => Url::base('https') . '/images/metafiles/Partner_524x273.webp',
+        ]); // twitter:image:src
+
+        $view->registerMetaTag([
+            'name' => 'twitter:image:alt',
+            'content' => Url::base('https') . '/images/metafiles/Partner_524x273.png',
+        ]); // twitter:image:alt
+
+        /*************** Open Graph Data (and facebook) ***************/
+        /* Open Graph Data (and facebook) */
+        $view->registerMetaTag([
+            'property' => 'og:title',
+            'content' => $title,
+        ]); // og:title
+
+        $view->registerMetaTag([
+            'property' => 'og:type',
+            'content' => 'website'
+        ]); // og:type
+
+        $view->registerMetaTag([
+            'property' => 'og:url',
+            'content' => Url::base('https') . Yii::$app->request->url,
+        ]); // og:url
+
+        $view->registerMetaTag([
+            'property' => 'og:image',
+            'content' => Url::base('https') . '/images/metafiles/Partner_524x273.png',
+        ]); // og:image
+
+        $view->registerMetaTag([
+            'property' => 'og:image:secure_url',
+            'content' => Url::base('https') . '/images/metafiles/Partner_524x273.png',
+        ]); // og:image:secure_url
+
+        $view->registerMetaTag([
+            'property' => 'og:image:type',
+            'content' => 'image/png',
+        ]); // og:image:type
+
+        $view->registerMetaTag([
+            'property' => 'image:width',
+            'content' => '1200',
+        ]); // image:width
+
+        $view->registerMetaTag([
+            'property' => 'image:height',
+            'content' => '630',
+        ]); // image:height
+
+        $view->registerMetaTag([
+            'property' => 'og:image:alt',
+            'content' => 'TSA News Logo',
+        ]); // og:image:alt
+
+        $view->registerMetaTag([
+            'property' => 'og:description',
+            'content' => $description,
+        ]); // og:description
+
+        $view->registerMetaTag([
+            'property' => 'og:site_name',
+            'content' => $title,
+        ]); // og:sitename
+	}
 }
