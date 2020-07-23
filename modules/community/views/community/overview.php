07105159dc287d6facc4f6f5b8e26cf62f481ebd
Birnchen <developer@tsa.gg>
2020-07-23 11:56:37 +0200

    add ads.txt for google

--- modules/community/views/community/overview.php
+++ modules/community/views/community/overview.php
@@ -41,7 +41,6 @@ $this->title = \app\modules\community\Module::t('overview', 'overview_header');
     <div class="container">
         <!-- User Overview -->
 		<?php echo Html::a(\app\modules\community\Module::t('overview', 'overview_users'), ["/community/user-overview"], ['class' => 'footer-link d-flex align-items-center', 'aria-label' => "User Overview"]); ?>
-
         <!-- Orga Overview -->
 		<?= Html::a(\app\modules\community\Module::t('overview', 'overview_organisations'), ["#"], ['class' => 'footer-link d-flex align-items-center', 'target'=>'_blank', 'aria-label' => "User Overview"]); ?>
 
