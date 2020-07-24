da98ac8f68a39fb52f62f11ca89eed1512a3fc51
dantwire <dantwire@germantrovo.com>
2020-07-24 11:19:07 +0200

    Add Style to Game Add

--- modules/user/views/account/addGame.php
+++ modules/user/views/account/addGame.php
@@ -15,47 +15,46 @@ $this->title = \app\modules\user\Module::t('addGameAccount', 'addGameAccount_hea
 
 ?>
 
-<div class="site-add-gameid container ">
-    <div class="addGameID bg-darkblue-2 p-5 my-5">
-        <h3><?= Html::encode($this->title) ?></h3>
+<div class="site-add-gameid">
+    <div class="inner-wrapper">
+        <div class="addGameID bg-darkblue-2 p-5 my-5 col-8">
+            <h3><?= Html::encode($this->title) ?></h3>
 
-        <?php $form = ActiveForm::begin([
-            'id' => 'edit-details-form',
-            'options' => [ 'class' => 'form-horizontal'],
-                'fieldConfig' => [
-                    'template' => "{label}\n<div class=\"col-lg-12\">{input}</div>\n<div class=\"col-lg-12\">{error}</div>",
-                    'labelOptions' => ['class' => 'col control-label'],
-                ],
-            ]); ?>
+            <?php $form = ActiveForm::begin([
+                'id' => 'edit-details-form',
+                'options' => [ 'class' => 'form-horizontal'],
+                    'fieldConfig' => [
+                        'template' => "{label}\n<div class=\"col-lg-12\">{input}</div>\n<div class=\"col-lg-12\">{error}</div>",
+                        'labelOptions' => ['class' => 'col control-label'],
+                    ],
+                ]); ?>
 
-            <div class="row">
-                <div class="col-md-6">
-					<?= $form->field($model, 'player_id')->textInput(['placeholder' => \app\modules\user\Module::t('addGameAccount', 'addGameAccount_gameAccountPlaceholder'), "class" => 'input-default form-control form-control-color']) ?>
-					<?= $form->field($model, 'game_id')->dropDownList($gamesList, ["class" => 'input-default form-control form-control-color', 'prompt' => \app\modules\user\Module::t('addGameAccount', 'addGameAccount_chooseYourGame')]) ?>
+                <div class="row">
+                    <div class="col-md-6">
+                        <?= $form->field($model, 'player_id')->textInput(['placeholder' => \app\modules\user\Module::t('addGameAccount', 'addGameAccount_gameAccountPlaceholder'), "class" => 'input-default form-control form-control-color']) ?>
+                        <?= $form->field($model, 'game_id')->dropDownList($gamesList, ["class" => 'input-default form-control form-control-color', 'prompt' => \app\modules\user\Module::t('addGameAccount', 'addGameAccount_chooseYourGame')]) ?>
+                    </div>
+                    <div class="col-md-6">
+                        <?= $form->field($model, 'visible')->checkbox() ?>
+
+                        <?= $form->field($model, 'platform_id')->dropDownList($platformList, ["class" => 'input-default form-control form-control-color', 'prompt' => \app\modules\user\Module::t('addGameAccount', 'addGameAccount_chooseYourPlatform')]) ?>
+
+                    </div>
                 </div>
-                <div class="col-md-6">
-					<?= $form->field($model, 'visible')->checkbox() ?>
-
-					<?= $form->field($model, 'platform_id')->dropDownList($platformList, ["class" => 'input-default form-control form-control-color', 'prompt' => \app\modules\user\Module::t('addGameAccount', 'addGameAccount_chooseYourPlatform')]) ?>
-
+                <div class="col-12">
+                    <div class="float-left">
+                        <?= Html::a(\app\modules\user\Module::t('addGameAccount', 'addGameAccount_backToProfile'), ['user/details', 'userId' => $currentUserID], ['class' => 'outline-btn btn btn-primary delete', 'name' => 'backToProfile-button']); ?>
+                    </div>
+                    <div class="float-right">
+                        <?= Html::submitButton(\app\modules\user\Module::t('addGameAccount', 'addGameAccount_save'), ['class' => 'filled-btn btn btn-primary', 'name' => 'saveGameId-button']) ?>
+                    </div>
+                    <div class="clearfix"></div>
                 </div>
-            </div>
-            <div class="col-12">
-                <div class="float-left">
-                    <?= Html::a(\app\modules\user\Module::t('addGameAccount', 'addGameAccount_backToProfile'), ['user/details', 'userId' => $currentUserID], ['class' => 'outline-btn btn btn-primary delete', 'name' => 'backToProfile-button']); ?>
-                </div>
-                <div class="float-right">
-                    <?= Html::submitButton(\app\modules\user\Module::t('addGameAccount', 'addGameAccount_save'), ['class' => 'filled-btn btn btn-primary', 'name' => 'saveGameId-button']) ?>
-                </div>
-                <div class="clearfix"></div>
-            </div>
+            <?php ActiveForm::end(); ?>
+        </div>
+        <div class="col-4">
+            <!-- Add Ad-Banner here -->
+        </div>
 
-
-
-
-
-
-       <?php ActiveForm::end(); ?>
-    
     </div>
 </div>
