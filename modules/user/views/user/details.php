9eafe3ccc18fa3867beff7d893e7d56ff3d8f936
dantwire <dantwire@germantrovo.com>
2020-07-24 23:01:21 +0200

    Add Show/Hide to Game Accounts

--- modules/user/views/user/details.php
+++ modules/user/views/user/details.php
@@ -345,7 +345,7 @@ $this->title = $userInfo['user_name'] . '\'s ' . \app\modules\user\Module::t('us
                                 </div>
                                 <?php foreach($platform['game'] as $game) : ?>
                                     <div class="px-5 row">
-                                        <div class="col-12 col-lg-6">
+                                        <div class="col-12 col-lg-8">
                                             <div class="game mb-4">
                                                 <div class="col-2 float-left">
                                                     <?= Html::img(Yii::$app->HelperClass->checkImage('/images/games/', $game['icon']) . '.webp',  ['class' => 'rounded-circle avatar-image', 'aria-label' => $platform['platformName']. '.webp', 'onerror' => 'this.src=\'' . Yii::$app->HelperClass->checkImage('/images/games/', $game['icon']) . '.png\''] ); ?>
@@ -355,14 +355,24 @@ $this->title = $userInfo['user_name'] . '\'s ' . \app\modules\user\Module::t('us
                                                         <h4 class="float-left"><?= ($game['visible'] || $userInfo['isMySelfe'])? $game['accountId'] : 'ID Not Public' ?></h4>
                                                         <?php if ($userInfo['isMySelfe']): ?>
                                                             <?php
-                                                                echo Html::a('',
+                                                                echo Html::a(($game['visible'] == 1 ? 
+                                                                '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-eye-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                                                    <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
+                                                                    <path fill-rule="evenodd" d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
+                                                                </svg>'
+                                                                : 
+                                                                '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-eye-slash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                                                    <path d="M10.79 12.912l-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7.029 7.029 0 0 0 2.79-.588zM5.21 3.088A7.028 7.028 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474L5.21 3.089z"/>
+                                                                    <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829l-2.83-2.829zm4.95.708l-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829z"/>
+                                                                    <path fill-rule="evenodd" d="M13.646 14.354l-12-12 .708-.708 12 12-.708.708z"/>
+                                                                </svg>'),
                                                                     [
                                                                         "account/toggle-game-account",
                                                                         "gameId" => $game['id'],
                                                                         "platformId" => $platform['id'],
                                                                         "userId" => $userInfo['user_id']
                                                                     ],
-                                                                    ['class' => $game['visible'] == 1 ? "filled-btn btn btn-primary upload" : "glyphicon glyphicon-eye-close glyphicon-game-account btn btn-primary upload",
+                                                                    ['class' => $game['visible'] == 1 ? "filled-btn btn btn-primary add-btn float-right" : "outline-btn btn btn-primary add-btn float-right",
                                                                         'title' => $game['visible'] == 1 ? \app\modules\user\Module::t('userDetails', 'userDetails_info_gameAccountVisible') : \app\modules\user\Module::t('userDetails', 'userDetails_info_gameAccountNotVisible')
                                                                     ]
                                                                 )
