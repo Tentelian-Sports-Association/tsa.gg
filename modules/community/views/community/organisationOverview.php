ea46977840090b55bd13188171227f08a6995bb6
dantwire <dantwire@germantrovo.com>
2020-07-27 11:07:33 +0200

    Add Organisation Overview Style

--- modules/community/views/community/organisationOverview.php
+++ modules/community/views/community/organisationOverview.php
@@ -14,7 +14,7 @@ use yii\widgets\LinkPager;
 
 \app\modules\community\assets\searchOverview\SearchOverviewAsset::register($this);
 
-$this->title = \app\modules\community\Module::t('overview', 'overview_header_users');
+$this->title = \app\modules\community\Module::t('overview', 'overview_header_organisations');
 
 $searchString = '';
 
@@ -54,6 +54,86 @@ sortedPaginatedOrganisation[$nr]['Language']['name']
                     Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und Konsonantien.
                 </p>
             </div>
+        </div>
+    </div>
+
+    <div class="inner-wrapper">
+        <div class="row">
+            <div class="col-md-10">
+                <div class="searchHeader">
+                    <div class="col-12 quickSearch">
+                        <?php $form = ActiveForm::begin(['id' => 'user-search-form', 'options' => ['class' => 'form-horizontal user-search-bar']]); ?>
+                            <div class="col-5 searchBox">
+                                <div class="vline_large"></div>
+                                <?= $form->field($searchModel, 'searchString')->textInput(['class' => 'input-default'])->label(false); ?>
+                            </div>
+
+                            <div class="col-6 sortBox">
+                                <div class="col-6 col-sortascend">
+                                    <?= $form->field($searchModel, 'sortAscend')->dropDownList($sortOrder, ['class' => 'select-wrapper input-default'])->label(false); ?>
+                                </div>
+                                <div class="vline_small"></div>
+                                <div class="col-6 col-sortedby">
+                                    <?= $form->field($searchModel, 'sortedBy')->dropDownList($sortOrderBy, ['class' => 'select-wrapper input-default'])->label(false); ?>
+                                </div>
+                            </div>
+                            <div class="col-1 search-button">
+                                <?= Html::submitButton(\app\modules\community\Module::t('searchForm', 'searchForm_btnSort'), ['class' => 'btn btn-primary delete search-btn', 'name' => 'search-button']) ?>
+                            </div>
+                            <div class="clearfix"></div>
+                        <?php ActiveForm::end(); ?>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <div class="float-right">
+                        <?php echo LinkPager::widget(['pagination' => $pagination]); ?>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="row users">
+                    <div class="col-12 col-md-12">
+                        <div class="col-12 userBodyHead">
+                            <div class="col-2 id float-left"><?= \app\modules\community\Module::t('searchForm', 'searchForm_byID') ?></div>
+                            <div class="col-3 username float-left"><?= \app\modules\community\Module::t('searchForm', 'searchForm_byName') ?></div>
+                            <div class="col-3 invite float-left"></div>
+                            <div class="col-2 natImg float-left"><?= \app\modules\community\Module::t('searchForm', 'searchForm_byNationality') ?></div>
+                            <div class="col-2 langImg float-left"><?= \app\modules\community\Module::t('searchForm', 'searchForm_byLanguage') ?></div>      
+                            <div class="clearfix"></div>
+                        </div>
+                        <div class="clearfix"></div>
+                    
+                        <?php foreach ($sortedPaginatedOrganisation as $index => $orga) : ?>
+                        <div class="col-12 userBody">
+                            <div class="col-2 id float-left">
+                                <?php echo $orga['ID']; ?>
+                            </div>
+                            <div class="col-3 float-left">
+                                <div class="avatar float-left">
+                                    <?= Html::img(Yii::$app->HelperClass->checkImage('/images/avatars/organisation/', $orga['ID']) . '.webp', ['aria-label' => $orga['Name']. '.webp', 'onerror' => 'this.src=\'' . Yii::$app->HelperClass->checkImage('/images/avatars/organisation/', $orga['ID']) . '.png\'']) ?>		
+                                </div>
+                                <div class="username float-left">
+                                    <?php echo $orga['Name']; ?>
+                                </div>
+                            </div>
+                            <div class="col-3 invite float-left"></div>
+                            <div class="col-2 natImg float-left">
+                                <?= Html::img(Yii::$app->HelperClass->checkNationalityImage($orga['Nationality']['icon'], '4x3'), ['aria-label' => 'nationality Image', 'alt' => $orga['Nationality']['name'],'class' => 'IMG']) ?>
+                            </div>
+                            <div class="col-2 langImg float-left">
+                                <?= Html::img(Yii::$app->HelperClass->checkNationalityImage($orga['Language']['icon'], '4x3'), ['aria-label' => 'nationality Image', 'alt' => $orga['Language']['name'],'class' => 'IMG']) ?>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <div class="float-right">
+                        <?php echo LinkPager::widget(['pagination' => $pagination]); ?>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2"></div>
         </div>
     </div>
 </div>
