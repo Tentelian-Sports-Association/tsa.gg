32df8a0a687c90f5c506d5173b0de99c3d504459
dantwire <dantwire@germantrovo.com>
2020-07-28 14:08:53 +0200

    Add Error Page Style

--- views/site/error.php
+++ views/site/error.php
@@ -10,18 +10,18 @@ use yii\helpers\Html;
 $this->title = $name;
 ?>
 <div class="site-error">
+    <div class="inner-wrapper">
+        <h1><?= Html::encode($this->title) ?></h1>
 
-    <h1><?= Html::encode($this->title) ?></h1>
+        <div class="alert alert-danger">
+            <?= nl2br(Html::encode($message)) ?>
+        </div>
 
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
+        <p>
+            The above error occurred while the Web server was processing your request.
+        </p>
+        <p>
+            Please contact us if you think this is a server error. Thank you.
+        </p>
     </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
-
 </div>
