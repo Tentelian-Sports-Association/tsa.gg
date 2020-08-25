f769c004730d6b81754398b3cbcb6e06696417d1
dantwire <dantwire@germantrovo.com>
2020-08-25 21:24:10 +0200

    Add active tournament site

--- modules/tournament/views/tournament/aktiveTournaments.php
+++ modules/tournament/views/tournament/aktiveTournaments.php
@@ -60,57 +60,60 @@ foreach(openTournamentList as $openTournament)
                 <div class="row openTournament desktop">
                     <div class="col-12 col-md-12">
                         <div class="col-12 openTournamentBodyHead">
-                            <div class="col-lg-2 tournamentName float-left"><?= \app\modules\tournament\Module::t('active', 'firstRow') ?></div>
-                            <div class="col-3 col-lg-3 tournamentStart float-left"><?= \app\modules\tournament\Module::t('active', 'secondRow') ?></div>
-                            <div class="col-3 col-lg-3 registerUntil float-left"><?= \app\modules\tournament\Module::t('active', 'thirdRow') ?></div>
-                            <div class="col-3 col-lg-2 checkIn float-left"><?= \app\modules\tournament\Module::t('active', 'fourthRow') ?></div>
-                            <div class="col-3 col-lg-3 buttons float-left"></div>
+                            <div class="col-5 col-lg-4 tournamentName float-left"><?= \app\modules\tournament\Module::t('active', 'firstRow') ?></div>
+                            <div class="col-2 col-lg-2 tournamentStart float-left"><?= \app\modules\tournament\Module::t('active', 'secondRow') ?></div>
+                            <div class="col-2 col-lg-2 registerUntil float-left"><?= \app\modules\tournament\Module::t('active', 'thirdRow') ?></div>
+                            <div class="col-2 col-lg-2 checkIn float-left"><?= \app\modules\tournament\Module::t('active', 'fourthRow') ?></div>
+                            <div class="col-1 col-lg-2 buttons float-left"></div>
                             <div class="clearfix"></div>
                         </div>
                         <div class="clearfix"></div>
                         <?php foreach ($openTournamentList as $openTournament) : ?>
                             <div class="col-12 openTournamentBody">
-                                <div class="col-3 col-lg-3 float-left">
+                                <div class="col-5 col-lg-4 float-left">
                                     <div class="username float-left">
                                         <?= Html::a($openTournament['Name'], ['details', 'gameId' => $choosedGame['id'], 'tournamentId' => $openTournament['ID']], ['class' => '']); ?>
                                     </div>
                                 </div>
-                                <div class="col-3 col-lg-3 invite float-left">
+                                <div class="col-2 col-lg-2 invite float-left">
                                     <?= $openTournament['DtStart'] ?>
                                 </div>
-                                <div class="col-3 col-lg-2 natImg float-left">
+                                <div class="col-2 col-lg-2 natImg float-left">
                                     <?= $openTournament['DtRegEnd'] ?>
                                 </div>
-                                <div class="col-3 col-lg-2 langImg float-left">
+                                <div class="col-2 col-lg-2 langImg float-left">
                                     <?= $openTournament['DtCheckIn'] ?>
                                 </div>
-                                <?php if($openTournament['RegisterOpen']) : ?>
+                                <div class="col-1 col-lg-2 langImg float-left">
+                                    <?php if($openTournament['RegisterOpen']) : ?>
+                                        <?php
+                                            echo Html::a('Register',
+                                                [
+                                                    "register",
+                                                    "gameId" => $choosedGame['id'],
+                                                    "tournamentId" => $openTournament['ID']
+                                                ],
+                                                ['class' => "filled-btn btn btn-primary",
+                                                    'title' => 'Register for Tournament'
+                                                ]
+                                            )
+                                        ?>
+                                    <?php elseif($openTournament['CheckInOpen']) : ?>
                                     <?php
-									    echo Html::a('Register',
-										    [
-											    "register",
-											    "gameId" => $choosedGame['id'],
-											    "tournamentId" => $openTournament['ID']
-										    ],
-										    ['class' => "filled-btn btn btn-primary",
-											    'title' => 'Register for Tournament'
-										    ]
-									    )
-								    ?>
-                                <?php elseif($openTournament['CheckInOpen']) : ?>
-                                <?php
-									    echo Html::a('Check-In',
-										    [
-											    "checkin",
-											    "gameId" => $choosedGame['id'],
-											    "tournamentId" => $openTournament['ID']
-										    ],
-										    ['class' => "filled-btn btn btn-primary",
-											    'title' => 'Check-In for Tournament'
-										    ]
-									    )
-								    ?>
-                                <?php endif; ?>
+                                            echo Html::a('Check-In',
+                                                [
+                                                    "checkin",
+                                                    "gameId" => $choosedGame['id'],
+                                                    "tournamentId" => $openTournament['ID']
+                                                ],
+                                                ['class' => "filled-btn btn btn-primary",
+                                                    'title' => 'Check-In for Tournament'
+                                                ]
+                                            )
+                                        ?>
+                                    <?php endif; ?>
+                                </div>
+                                <div class="clearfix"></div>
                             </div>
                         <?php endforeach; ?>
                     </div>
