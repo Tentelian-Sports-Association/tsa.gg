1b73c5051fc263adbff32ddf09f236b0141be51d
dantwire <dantwire@germantrovo.com>
2020-08-29 10:43:52 +0200

    Add Styles for Buttons on Team Edit

--- modules/team/views/team/details.php
+++ modules/team/views/team/details.php
@@ -129,14 +129,16 @@ teamManager['Name']
                                                     <?= Html::a( $member['UserName'], ['/user/details', 'userId' => $member['UserID']], ['class' => '']); ?>
                                                     <?php if ($isOwner) : ?>
                                                         <?php
-                                                            /** hieraus ein schönes x machen zum löschen */
-                                                            echo Html::a('X',
+                                                            /** hieraus ein schï¿½nes x machen zum lï¿½schen */
+                                                            echo Html::a('<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-x" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                                            <path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                                                          </svg>',
                                                                 [
                                                                     "remove-from-team",
                                                                     "teamId" => $team['ID'],
                                                                     "userId" => $member['UserID']
                                                                 ],
-                                                                ['class' => "invite-btn",
+                                                                ['class' => "filled-btn btn btn-danger add-btn",
                                                                     'title' => 'invite'
                                                                 ]
                                                             )
@@ -153,14 +155,16 @@ teamManager['Name']
                                                             <?php if($role['RoleID'] >= 3) : ?>
                                                                 <?php if ($isOwner) : ?>
                                                                     <?php
-                                                                        /** hieraus ein schönes x machen zum löschen */
-                                                                        echo Html::a('change',
+                                                                        /** hieraus ein schï¿½nes x machen zum lï¿½schen */
+                                                                        echo Html::a('<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                                                        <path fill-rule="evenodd" d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+                                                                      </svg>',
                                                                             [
                                                                                 "change-player-sub-state",
                                                                                 "teamId" => $team['ID'],
                                                                                 "userId" => $member['UserID']
                                                                             ],
-                                                                            ['class' => "invite-btn",
+                                                                            ['class' => "filled-btn btn btn-primary add-btn",
                                                                                 'title' => 'invite'
                                                                             ]
                                                                         )
