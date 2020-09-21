2a69b90036152c25da0463f15814eab92b1cc5f1
Birnchen <developer@tsa.gg>
2020-09-21 16:04:17 +0200

    added page for each action to seperate them action technical and game specific

---
+++ modules/tournament/modules/clashRoyale/views/registration_player.php
@@ -0,0 +1,10 @@
+<?php
+/* @var $this yii\web\View */
+
+use yii\bootstrap4\ActiveForm;
+use yii\helpers\Html;
+use app\widgets\Alert;
+
+\app\modules\tournament\assets\TournamentRegistrationAsset::register($this);
+
+?>
